e856f83f430f3a9b30f61614c0d3c47bdcb79e3b
mohammadHaris944 <mohammad.haris@devbox.co>
2020-09-08 17:04:36 +0500

    User Roles

---
+++ app/assets/javascripts/core-scripts.js
@@ -0,0 +1,12 @@
+// ** CORE
+// = require jquery/dist/jquery.min.js
+// = require popper.js/dist/umd/popper.js
+// = require bootstrap/dist/js/bootstrap.min.js
+
+// = require simplebar/dist/simplebar.min.js
+
+// = require dom-factory/dist/dom-factory.js
+// = require stack/plugins/list.js
+// = require stack/plugins/quill.js
+
+// = require material-design-kit/dist/material-design-kit.js
---
+++ app/assets/javascripts/stack/core-scripts.js
@@ -0,0 +1,19 @@
+
+
+//** APP SETTINGS
+//= require stack/settings.js
+
+//** PLUGIN WRAPPERS & INITS
+//= require stack/plugins/chartjs-rounded-bar.js
+//= require stack/plugins/charts.js
+//= require stack/plugins/daterangepicker.js
+//= require stack/plugins/dragula.js
+//= require stack/plugins/dropzone.js
+//= require stack/plugins/flatpickr.js
+//= require stack/plugins/fullcalendar.js
+//= require stack/plugins/list.js
+//= require stack/plugins/quill.js
+//= require stack/plugins/toastr.js
+//= require stack/plugins/vector-maps.js
+
+//window['moment-range'].extendMoment(moment);
