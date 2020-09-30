88af926477b2b9114ea339c4dffdb7ea2e0477eb
Ammad Aslam <ammad.aslam@devbox.co>
2020-09-30 19:48:58 +0500

    Assign Admin in ability class

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -21,14 +21,14 @@ class Ability
     #   can :manage, User
     # end
 
-    # user.roles.each do |role|
-    #   if role == "Technician"
-    #     can :read, :all
-    #   else
-    #     can :manage, :all
-    #   end
-    # end
-    #
+    user.roles.each do |role|
+      if role == "Technician"
+        can :read, :all
+      else
+        can :manage, :all
+      end
+    end
+    
     # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
