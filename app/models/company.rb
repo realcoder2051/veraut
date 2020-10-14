1191c22e6b519623d491185c333f0010f7f6ac86
mohammadHaris944 <mohammad.haris@devbox.co>
2020-10-14 18:52:32 +0500

    Changes in COntacts

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,6 +1,5 @@
 class Company < ApplicationRecord
-	validates :entity_type , :payroll_frequency, presence: true
+	validates :entity_type ,:fiscal_year_end, :payroll_frequency, presence: true
 	validates_length_of :ein, :maximum => 9
-
 
 end
