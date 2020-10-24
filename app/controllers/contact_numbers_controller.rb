50ce60cfa6fa97e71772b2f313560585fca5262b
mohammadHaris944 <mohammad.haris@devbox.co>
2020-10-24 19:37:20 +0500

    Modification in Contact Number

--- app/controllers/contact_numbers_controller.rb
+++ app/controllers/contact_numbers_controller.rb
@@ -6,12 +6,17 @@ class ContactNumbersController < InheritedResources::Base
     contact_number[:user_id] = current_user.id
     if contact_number.contact_type == ""
       session[:error] = "Your choices have been saved, however the step can not be completed because there are additional required fields."
-    end
-    if contact_number.save
-			redirect_to generals_path
+      contact_number.save
+      redirect_to generals_path
     else
-      @number = ContactNumber.new
-			render :new
+      if check_contact_number_already_exist?(contact_number)
+        session[:error] = "Contact Number with this Contact type already exist"
+        @number = ContactNumber.new
+        render :new
+      else
+        contact_number.save
+			  redirect_to generals_path
+      end
     end
   end
 
@@ -26,14 +31,19 @@ class ContactNumbersController < InheritedResources::Base
   def update
     @contact_number = ContactNumber.find(params[:id])
     if params[:contact_number][:contact_type] == ""
+      @contact_number.update_attributes(contact_number_params)
       session[:error] = "Your choices have been saved, however the step can not be completed because there are additional required fields."
-    end
-    if @contact_number.update_attributes(contact_number_params)
       redirect_to generals_path
     else
-      render :edit
+      if check_contact_number_is_exist?
+        session[:error] = "Contact Number with this Contact type already exist"
+        @number = @contact_number
+        render :edit
+      else
+        @contact_number.update_attributes(contact_number_params)
+			  redirect_to generals_path
+      end
     end
-    
   end
 
   def index
@@ -46,12 +56,22 @@ class ContactNumbersController < InheritedResources::Base
     if contact_number.update_attribute(:active, true)
 			redirect_to generals_path
 		end
-	end
+  end
 
   private
 
     def contact_number_params
       params.require(:contact_number).permit(:number, :contact_type, :general_id)
+    end
+
+    def check_contact_number_already_exist?(contact_number)
+      result = ContactNumber.where("number = ? and contact_type = ? and user_id = ? and active = ?",contact_number.number,contact_number.contact_type,contact_number.user_id,false)
+      result.count > 0 ? true : false
+    end
+
+    def check_contact_number_is_exist?
+      result = ContactNumber.where("number = ? and contact_type = ? and active = ?",params[:contact_number][:number],params[:contact_number][:contact_type],false)
+      result.count > 0 ? true : false
     end
 
 end
